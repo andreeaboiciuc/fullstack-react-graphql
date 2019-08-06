c891b745ea42a389dd3000529fe095ac0b4eb71e
andreeaboiciuc <boiciuc.andreea@icloud.com>
2019-08-06 15:16:39 -0700

    mutation and query, schema

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,27 @@
-const mutations = {
-    createDog(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
 
-        const newDog = { name: args.name };
-        global.dogs.push(newDog);
-        console.log(args);
-        return newDog;
+        const item = await ctx.db.mutation.createItem(
+        {
+            data: {
+              ...args, 
+            },
+        },
+        info
+        );
+
+        console.log(item);
+
+        return item;
     }
+   // createDog(parent, args, ctx, info) {
+     //   global.dogs = global.dogs || [];
+
+       // const newDog = { name: args.name };
+       // global.dogs.push(newDog);
+       // console.log(args);
+       // return newDog;
+   // }
 };
 
-module.exports = mutations;
+module.exports = Mutations;
