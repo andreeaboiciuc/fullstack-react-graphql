943bdd280fea995bb3203f524aee618d4283b186
andreeaboiciuc <boiciuc.andreea@icloud.com>
2019-08-08 19:16:42 -0700

    fixing error UserCreateOneInput

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,27 +1,27 @@
 const Mutations = {
     async createItem(parent, args, ctx, info) {
-
-        const item = await ctx.db.mutation.createItem(
+      // TODO: Check if they are logged in
+  
+      const item = await ctx.db.mutation.createItem(
         {
-            data: {
-              ...args, 
-            },
+          data: {
+            ...args,
+          },
         },
         info
-        );
-
-        console.log(item);
-
-        return item;
-    }
-   // createDog(parent, args, ctx, info) {
-     //   global.dogs = global.dogs || [];
-
-       // const newDog = { name: args.name };
-       // global.dogs.push(newDog);
-       // console.log(args);
-       // return newDog;
-   // }
-};
-
-module.exports = Mutations;
+      );
+  
+      console.log(item);
+  
+      return item;
+    },
+    // createDog(parent, args, ctx, info) {
+    //   global.dogs = global.dogs || [];
+    //   // create a dog
+    //   const newDog = { name: args.name };
+    //   global.dogs.push(newDog);
+    //   return newDog;
+    // },
+  };
+  
+  module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,13 +1,23 @@
+//const Query = {
+//    dogs(parent, args, ctx, info) {
+//        global.dogs = global.dogs || [];
+
+//        return global.dogs;
+//    }
+ //   items() {
+ //       return [{ id: 'xablau',  title: 'foo'}]
+ //   }
+//};
+//module.exports = Query;
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
-    items: forwardTo('db')
- //   async items(parent, args, ctx, info) {
- //       console.log('getting Items!!');
- //       const items = await ctx.db.query.items();
- //       return items;
-
- //   },
+  items: forwardTo('db'),
+//   async items(parent, args, ctx, info) {
+//     console.log('Getting Items!!');
+//     const items = await ctx.db.query.items();
+//     return items;
+ //   }
 };
 
 module.exports = Query;
