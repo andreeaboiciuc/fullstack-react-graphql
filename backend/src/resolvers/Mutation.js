bf6d480859ec7566fa21b9cb2b385e1de060e785
andreeaboiciuc <boiciuc.andreea@icloud.com>
2019-08-09 15:06:29 -0700

    deleting items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,5 @@
 const Mutations = {
     async createItem(parent, args, ctx, info) {
-      // TODO: Check if they are logged in
   
       const item = await ctx.db.mutation.createItem(
         {
@@ -16,19 +15,23 @@ const Mutations = {
       return item;
     },
     updateItem(parent, args, ctx, info) {
-        console.log(ctx.db.mutation)
-        const updates = { ...args };
-        delete updates.id;
-        return ctx.db.mutation.updateItem(
-          {
-            data: updates,
-            where: {
-              id: args.id,
-            },
+      const updates = { ...args };
+      delete updates.id;
+      return ctx.db.mutation.updateItem(
+        {
+          data: updates,
+          where: {
+            id: args.id,
           },
-          info
-        );
-      },
-    };
+        },
+        info
+      );
+    },
+    async deleteItem(parent, args, ctx, info) {
+      const where = { id: args.id };
+      const item = await ctx.db.query.item({ where }, `{ id title}`);
+      return ctx.db.mutation.deleteItem({ where }, info);
+    },
+  };
   
   module.exports = Mutations;
