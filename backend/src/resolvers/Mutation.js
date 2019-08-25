30f551fce6904a936f5a4feef0ded6003fd7067f
andreeaboiciuc <boiciuc.andreea@icloud.com>
2019-08-25 00:17:34 -0700

    Sign Out button

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -75,6 +75,10 @@ const Mutations = {
       });
       return user;
     },
+    signout(parent, args, ctx, info) {
+      ctx.response.clearCookie('token');
+      return { message: 'goodbye' };
+    }, 
   };
      
   
