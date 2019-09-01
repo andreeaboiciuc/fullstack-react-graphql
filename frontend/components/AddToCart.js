da46e01767a58834b0af67bc1d8274c73cbe67b4
andreeaboiciuc <boiciuc.andreea@icloud.com>
2019-09-01 20:13:55 +0300

    displaying cart items and total

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
+
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -21,7 +23,11 @@ class AddToCart extends React.Component {
           id,
         }}
       >
-        {addToCart => <button onClick={addToCart}>Add To Cart 🛒</button>}
+        {(addToCart, { loading }) => (
+          <button disabled={loading} onClick={addToCart}>
+            Add{loading && 'ing'} To Cart 🛒
+          </button>
+        )}
       </Mutation>
     );
   }
