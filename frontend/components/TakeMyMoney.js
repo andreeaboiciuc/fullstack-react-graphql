dc2744c84d4304379462e38e94cbaf4df39269cb
andreeaboiciuc <boiciuc.andreea@icloud.com>
2019-09-05 19:13:03 +0300

    Saving Orders to the Database

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -1,73 +1,73 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import { Mutation, renderToStringWithData } from 'react-apollo';
+import { Mutation } from 'react-apollo';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
-import calcTotalPrice from  '../lib/calcTotalPrice';
+import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
-//import { create } from 'handlebars';
 
 const CREATE_ORDER_MUTATION = gql`
-    mutation createOrder($token: String! ) {
-        createOrder(token: $token) {
-            id
-            charge
-            total
-            items {
-                id
-                title
-            }
-        }
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
     }
+  }
 `;
 
 function totalItems(cart) {
-    return cart.reduce((tally, CartItem) => tally +
-    CartItem.quantity, 0);
+  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 }
 
 class TakeMyMoney extends React.Component {
-    onToken = (res, createOrder) => {
-        console.log('On Token called!');
-        console.log(res.id);
-
-        createOrder({
-            variables: {
-                token: res.id,
-            },
-        }).catch(err => {
-            alert(err.message);
-        });
-    };
-    render() {
-        return (
-          <User>
-            {({ data: { me } }) => (
-              <Mutation
-                mutation={CREATE_ORDER_MUTATION}
-                refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+  onToken = async (res, createOrder) => {
+    console.log('On Token Called!');
+    console.log(res.id);
+    // manually call the mutation once we have the stripe token
+    const order = await createOrder({
+      variables: {
+        token: res.id,
+      },
+    }).catch(err => {
+      alert(err.message);
+    });
+    console.log(order);
+  };
+  render() {
+    return (
+      <User>
+        {({ data: { me } }) => (
+          <Mutation
+            mutation={CREATE_ORDER_MUTATION}
+            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+          >
+            {createOrder => (
+              <StripeCheckout
+                amount={calcTotalPrice(me.cart)}
+                name="Sick Fits"
+                description={`Order of ${totalItems(me.cart)} items!`}
+                image={me.cart.length && me.cart[0].item && me.cart[0].item.image}
+                stripeKey="pk_test_HYwIPsjOgXL7BTssY9Qyl69Y00wCJJ9Npf"
+                currency="USD"
+                email={me.email}
+                token={res => this.onToken(res, createOrder)}
               >
-                {createOrder => (
-                  <StripeCheckout
-                    amount={calcTotalPrice(me.cart)}
-                    name="Sick Fits"
-                    description={`Order of ${totalItems(me.cart)} items!`}
-                    image={me.cart[0].item && me.cart[0].item.image}
-                    stripeKey="pk_test_HYwIPsjOgXL7BTssY9Qyl69Y00wCJJ9Npf"
-                    currency="USD"
-                    email={me.email}
-                    token={res => this.onToken(res, createOrder)}
-                  >
-                    {this.props.children}
-                  </StripeCheckout>
-                )}
-              </Mutation>
+                {this.props.children}
+              </StripeCheckout>
             )}
-          </User>
-        );
-      }
-    }
+          </Mutation>
+        )}
+      </User>
+    );
+  }
+}
+
 export default TakeMyMoney;
